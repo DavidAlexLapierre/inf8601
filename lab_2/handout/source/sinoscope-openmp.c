04e648ec062bf388cd9a800f33272c5c8792a348
JonathanBordeleau <jonathan.bordeleau@polymtl.ca>
2021-10-31 16:39:43 -0400

    JB D:

--- lab_2/handout/source/sinoscope-openmp.c
+++ lab_2/handout/source/sinoscope-openmp.c
@@ -12,7 +12,7 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
         goto fail_exit;
     }
     
-    #pragma omp parallel for simd collapse(2)
+    #pragma omp parallel for simd collapse(2) schedule(dynamic)
     for (int i = 0; i < sinoscope->width; i++) {
         for (int j = 0; j < sinoscope->height; j++) {
             float px    = sinoscope->dx * j - 2 * M_PI;
@@ -30,12 +30,12 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
             pixel_t pixel;
             color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
 
-            int index = (i * 3) + (j * 3) * sinoscope->width;
+            int index = (i * 3) + (j * 3) * sinoscope->widthcollapse(2);
 
             sinoscope->buffer[index + 0] = pixel.bytes[0];
             sinoscope->buffer[index + 1] = pixel.bytes[1];
             sinoscope->buffer[index + 2] = pixel.bytes[2];
-        }
+        } 
     }
 
     return 0;
