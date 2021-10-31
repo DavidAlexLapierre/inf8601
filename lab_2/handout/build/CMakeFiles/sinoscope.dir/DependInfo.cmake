9f620a732a6f8464adf42d1fdc652b721f4b6c0c
JonathanBordeleau <jonathan.bordeleau@polymtl.ca>
2021-10-31 15:52:18 -0400

    JB D: OpenMP seems ok

---
+++ lab_2/handout/build/CMakeFiles/sinoscope.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/color.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/color.c.o"
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/headless.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/headless.c.o"
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/image.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/image.c.o"
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/main.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/main.c.o"
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/opencl.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/opencl.c.o"
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/sinoscope-opencl.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/sinoscope-opencl.c.o"
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/sinoscope-openmp.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/sinoscope-openmp.c.o"
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/sinoscope-serial.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/sinoscope-serial.c.o"
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/sinoscope.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/sinoscope.c.o"
+  "/home/jonathan/Desktop/inf8601/lab_2/handout/source/viewer.c" "/home/jonathan/Desktop/inf8601/lab_2/handout/build/CMakeFiles/sinoscope.dir/source/viewer.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CL_TARGET_OPENCL_VERSION=220"
+  "__KERNEL_FILE__=\"/home/jonathan/Desktop/inf8601/lab_2/handout/source/kernel/sinoscope.cl\""
+  "__OPENCL_INCLUDE__=\"/home/jonathan/Desktop/inf8601/lab_2/handout/source/kernel\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab_2/handout/build/CMakeFiles/sinoscope.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sinoscope.dir/source/color.c.o"
+  "CMakeFiles/sinoscope.dir/source/headless.c.o"
+  "CMakeFiles/sinoscope.dir/source/image.c.o"
+  "CMakeFiles/sinoscope.dir/source/main.c.o"
+  "CMakeFiles/sinoscope.dir/source/opencl.c.o"
+  "CMakeFiles/sinoscope.dir/source/sinoscope-opencl.c.o"
+  "CMakeFiles/sinoscope.dir/source/sinoscope-openmp.c.o"
+  "CMakeFiles/sinoscope.dir/source/sinoscope-serial.c.o"
+  "CMakeFiles/sinoscope.dir/source/sinoscope.c.o"
+  "CMakeFiles/sinoscope.dir/source/viewer.c.o"
+  "sinoscope"
+  "sinoscope.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sinoscope.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
